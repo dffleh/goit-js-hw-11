eb75ea6cb5cb03b3e95023af0031ae41bf2f2ad0
dffleh <dffleh1995@gmail.com>
2022-09-18 00:41:48 +0300

    fix bug

--- src/fetchFoto.js
+++ src/fetchFoto.js
@@ -4,29 +4,11 @@ const URL = 'https://pixabay.com/api/';
 const KEY = '29948527-2314e3f34b81248b29ec7b1b5';
 const PARAMETR = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=';
 
-export async function fetchFoto(submitValue,pageforBtn) {
+export async function fetchFoto(submitValue, pageforBtn) {
     try {
-        return await axios.get(`${URL}?key=${KEY}&q=${submitValue}${PARAMETR}${pageforBtn}`).then(response => response.data);
-        
+        const response = await axios.get(`${URL}?key=${KEY}&q=${submitValue}${PARAMETR}${pageforBtn}`);
+        return response.data;
     } catch (error) {
         console.error(error);
     }
-    
-    // return fetch(`${URL}?key=${KEY}&q=${q}${PARAMETR}`).then(response => {
-    //     if (!response.ok || response.status === 404) {
-    //         throw new Error(response.status);
-    //     }
-    //     return response.json();
-    // });
 };
-
-// https://pixabay.com/api/?key=29948527-2314e3f34b81248b29ec7b1b5&q=yellow+flowers&image_type=photo
-// ${URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageforBtn}
-// async function getUser() {
-//     try {
-//       const response = await axios.get('/user?ID=12345');
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//  }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-// import './css/styles.css'
-
-import scroll from "./scroll";
 import Notiflix from "notiflix";
 import { getRefs } from "./getRefs";
 import { fetchFoto } from "./fetchFoto";
 import { clearGalleryDiv, renderGalleryDiv } from "./renderFunction";
 
-
-
 const refs = getRefs();
-
-// let lightbox = new SimpleLightbox('.photo-link', {
-//   captions: true,
-//   captionDelay: 200,
-//   captionsData: 'alt',
-// });
 
 let pageforBtn = 1;
 let totalHitsValue = '';
@@ -34,12 +23,12 @@ async function onSubmit(event) {
         clearGalleryDiv();
         return;
     }
-    const response = await fetchFoto(submitValue,pageforBtn)
+    const response = await fetchFoto(submitValue, pageforBtn)
     currentHits = response.hits.length;
     console.dir(response);
-    
+
     if (response.totalHits > 0) {
-        clearGalleryDiv() 
+        clearGalleryDiv()
         Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`)
         Notiflix.Notify.success('Slava Ukraine');
         renderGalleryDiv(response.hits);
@@ -47,28 +36,27 @@ async function onSubmit(event) {
 
     if (response.totalHits > 40) {
         refs.loadMoreBtn.classList.remove('is-hidden');
-      } else {
+    } else {
         refs.loadMoreBtn.classList.add('is-hidden');
-      }
+    }
 
 
     if (response.totalHits === 0) {
         clearGalleryDiv();
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        Notiflix.Notify.success('Slava Ukraine'); 
+        Notiflix.Notify.success('Slava Ukraine');
         refs.loadMoreBtn.classList.add('is-hidden');
-     }
+    }
 }
 
 async function loadMore() {
-    pageforBtn +=1;
+    pageforBtn += 1;
     const response = await fetchFoto(submitValue, pageforBtn);
     renderGalleryDiv(response.hits);
-    currentHits += response.hits.length;
+    totalHitsValue += response.hits.length;
 
-    if (currentHits >= response.totalHits) {
+    if (totalHitsValue >= response.totalHits) {
         refs.loadMoreBtn.classList.add('is-hidden');
         Notify.info("We're sorry, but you've reached the end of search results.");
-      }
+    }
 }
-
